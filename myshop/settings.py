ba29e4468ab30116ed0fce5d32dc6d36eaa4caf0
XXXDESHXXX <kirya.shvarts@list.ru>
2024-02-07 11:36:16 +0300

    Create cart.py; Create class Cart; Add cart urls into urls.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -20,6 +20,9 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+CART_SESSION_ID = 'cart'
+
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -45,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
